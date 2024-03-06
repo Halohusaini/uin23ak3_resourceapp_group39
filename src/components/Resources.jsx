56395c408cf859c2f2b9b00a91db0500f4bfa474
Samatara <samatarbulbula@gmail.com>
2024-03-06 13:29:57 +0100

    resourceapp
    jeg la til usenavigation for å hente ut første categorien, men selvfølgelig funka det ikke oof

--- src/components/Resources.jsx
+++ src/components/Resources.jsx
@@ -1,11 +1,17 @@
-import React from 'react';
-import { useParams } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { useParams, useNavigate } from 'react-router-dom';
 import resources from '../resources/resources'; // Adjust the path as needed
 import NavigationLink from './NavigationLink';
+
 
 export default function Resources() {
   const { category } = useParams();
   let content = resources.filter(res => res.category === category.toLowerCase());
+ 
+if (!content)  {
+  return resources(res => res.category[0])
+}
+
 
   return (
     <section id='content'>
