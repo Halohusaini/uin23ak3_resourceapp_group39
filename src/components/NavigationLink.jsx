e64245375390c53639394cb7326ddc6914adbd4b
Halohusaini <hallohusaini56@gmail.com>
2024-03-06 13:00:43 +0100

    Gjorde endringer i Main.scss på root og main section
    Endret main id til root som fikset problemet med borders

--- src/components/NavigationLink.jsx
+++ src/components/NavigationLink.jsx
@@ -4,26 +4,8 @@ import { Link } from 'react-router-dom';
 
 export default function NavigationLink({ source }) {
     return (
-        <li>
+        <li id>
             <a href={source.url} target="_blank" rel="noopener noreferrer">{source.title}</a>
         </li>
     );
 }
-
-
-/* // Inside NavigationLink.jsx
-
-import React from 'react';
-
-const NavigationLink = ({ source }) => {
-  return (
-    <li>
-      <a href={source.url} target="_blank" rel="noopener noreferrer">
-        {source.title}
-      </a>
-    </li>
-  );
-};
-
-export default NavigationLink;
-*/
