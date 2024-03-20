b1bd94a18a3b9b484b3869a1b2563a3e8fdebd78
Halohusaini <hallohusaini56@gmail.com>
2024-03-20 12:37:33 +0100

    Update  MainNavigation.jsx
    Jeg bruker map for å mappe ut bare category string fra ressurs.js for at den ikke tar med URL og title.

--- src/components/MainNavigation.jsx
+++ src/components/MainNavigation.jsx
@@ -1,38 +1,44 @@
 import React, { useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom"; 
+import { useNavigate } from "react-router-dom";
+import resources from "../resources/resources";
 
 export default function MainNavigation() {
-    const [categories, setCategories] = useState([]);
-    const [activeCategory, setActiveCategory] = useState("");
-    const navigate = useNavigate(); 
+  const [categories, setCategories] = useState([]);
+  const [activeCategory, setActiveCategory] = useState("");
+  const navigate = useNavigate();
 
-    useEffect(() => {
-        const uniqueCategories = resources.reduce((acc, current) => {
-            const x = acc.find(item => item.category === current.category);
-            if (!x) {
-                return acc.concat([current]);
-            } else {
-                return acc;
-            }
-        })
-    });
+  useEffect(() => {
+    const uniqueCategories = resources
+      .reduce((acc, current) => {
+        const x = acc.find((item) => item.category === current.category);
+        if (!x) {
+          return acc.concat([current]);
+        } else {
+          return acc;
+        }
+      }, [])
+      .map((resources) => resources.category);
 
-    const handleCategoryClick = (category) => {
-        setActiveCategory(category);
-        navigate(`/resources/${category}`); 
-    };
+    setCategories(uniqueCategories);
+    setActiveCategory(uniqueCategories[0]);
+  });
 
-    return (
-        <nav id="tab-container">
-            {categories.map((category) => (
-                <button
-                    key={category}  
-                    className={`btn ${activeCategory === category ? 'active' : ''}`}
-                    onClick={() => handleCategoryClick(category)} 
-                >
-                    {category}
-                </button>
-            ))}
-        </nav>
-    );
+  const handleCategoryClick = (category) => {
+    setActiveCategory(category);
+    navigate(`/resources/${category}`);
+  };
+
+  return (
+    <nav id="tab-container">
+      {categories.map((category) => (
+        <button
+          key={category}
+          className={`btn ${activeCategory === category ? "active" : ""}`}
+          onClick={() => handleCategoryClick(category)}
+        >
+          {category}
+        </button>
+      ))}
+    </nav>
+  );
 }
