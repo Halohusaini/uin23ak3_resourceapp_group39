b629b954281359528bdf40d4ce2e11e57698cec6
miran-nihad <miran-walashy@hotmail.com>
2024-03-20 12:31:32 +0100

    Update MainNavigation.jsx
    vi hadde lagd en static array av categoriene, så vi hadde ikke hentet ressursene fra selve ressurs.js filen. Bruker reduce metoden for å slette duplikater i ressurs.js også laget en variabel x som går gjennom categoriene og finner de current categoriene som er i ressurs.js. IF testen returnerer categoriene.

--- src/components/MainNavigation.jsx
+++ src/components/MainNavigation.jsx
@@ -1,10 +1,21 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom"; 
 
 export default function MainNavigation() {
-    const categories = ["HTML", "CSS", "JavaScript", "React", "headless-cms"];
-    const [activeCategory, setActiveCategory] = useState(categories[0]);
+    const [categories, setCategories] = useState([]);
+    const [activeCategory, setActiveCategory] = useState("");
     const navigate = useNavigate(); 
+
+    useEffect(() => {
+        const uniqueCategories = resources.reduce((acc, current) => {
+            const x = acc.find(item => item.category === current.category);
+            if (!x) {
+                return acc.concat([current]);
+            } else {
+                return acc;
+            }
+        })
+    });
 
     const handleCategoryClick = (category) => {
         setActiveCategory(category);
